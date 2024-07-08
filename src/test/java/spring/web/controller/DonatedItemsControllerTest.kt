26617093431659e041a17cc8f90fd4bc0d04abe1
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-07-08 19:29:58 -0300

    update test donated items

--- src/test/java/spring/web/controller/DonatedItemsControllerTest.kt
+++ src/test/java/spring/web/controller/DonatedItemsControllerTest.kt
@@ -1,15 +1,25 @@
 package spring.web.controller
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import spring.commons.DonateItemsConstains.ItemsValid
+import spring.domain.services.DonatedItemsService
 
-class DonatedItemsControllerTest (
-//    private val donatedItemsController: DonatedItemsController
-) {
+class DonatedItemsControllerTest () {
+
+    private lateinit var donatedItemsService: DonatedItemsService
+
+    @BeforeEach
+    fun setup (){
+        val service = DonatedItemsService()
+        donatedItemsService = service
+    }
+
     @Test
     fun testDonatedItemsController() {
         val donatedItemsController = DonatedItemsController()
-        donatedItemsController.insert(ItemsValid)
-
+        val result = donatedItemsController.insert(ItemsValid)
+        assertThat(result.id).isNotNull()
     }
 }
